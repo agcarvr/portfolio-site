c0b26efd4103ac253ec81fb0fe81fd1e6319719a
aaroncarver <agcarvr@gmail.com>
2021-05-10 11:04:43 -0600

    created carousel with links to projects

---
+++ src/Components/ImageSlider/index.js
@@ -0,0 +1,41 @@
+import { SliderData } from "../Sliderdata";
+import {useState} from 'react';
+import {FaArrowAltCircleRight, FaArrowAltCircleLeft} from 'react-icons/fa';
+import styles from './slider.module.css'
+
+
+export default function ImageSlider({slides}){
+    const [current, setCurrent] = useState(0)
+    const length = slides.length
+    
+    const nextSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current + 1)
+    };
+
+    const prevSlide = () => {
+        setCurrent(current === 0 ? length - 1 : current - 1)
+    }
+
+    if(!Array.isArray(slides) || slides.length <= 0){
+        return null;
+    }
+
+    return(
+        <section className={styles.slider}>
+            <FaArrowAltCircleLeft className={styles.leftarrow} onClick={prevSlide} />
+
+            <FaArrowAltCircleRight className={styles.rightarrow} onClick={nextSlide}/>
+
+            {SliderData.map((slide, idx) => {
+                return (
+                    <div className={idx === current ? styles.slideActive : styles.slide} key={idx}>
+
+                        {idx === current && (<a href={slide.url} target="_blank" rel="norefferer"><img src={slide.image} className={styles.image}/> </a>)}
+
+                    </div>
+                )
+            })}
+
+        </section>
+    )
+}
---
+++ src/Components/Sliderdata/index.js
@@ -0,0 +1,19 @@
+import betterquest from '../../images/betterquest.png'
+import freewebbooks from '../../images/freewebbooks.png'
+import oneshotrpg from '../../images/oneshotrpg.png'
+
+
+export const SliderData= [
+    {
+        image: freewebbooks,
+        url: "https://agcarvr.github.io/Free-Web-Books/"
+    },
+    {
+        image: betterquest,
+        url: "https://betterquest.herokuapp.com/"
+    },
+    {
+        image: oneshotrpg,
+        url: "https://agcarvr.github.io/one-shot-rpg/"
+    }
+]
