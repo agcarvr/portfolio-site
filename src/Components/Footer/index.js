fd4e286b2020bed0bbb2a4031bab4b748d2d115e
aaroncarver <agcarvr@gmail.com>
2021-05-14 10:05:53 -0600

    fixed continuity error in footer

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -3,8 +3,8 @@ import styles from './footer.module.css';
 export default function Footer(){
     return(
         <div className={styles.footerbody}>
-            <a className={styles.defaultlink} href="https://paypal.me/erincarver926" target="_blank" rel="noreferrer">Donate to my coffee fund</a>
-            <a className={styles.defaultlink} href="https://www.twitch.tv/webdeverin" target="_blank" rel="noreferrer">View my coding streams</a>
+            <a className={styles.defaultlink} href="https://paypal.me/erincarver926" target="_blank" rel="noreferrer">Donate to Erin's coffee fund</a>
+            <a className={styles.defaultlink} href="https://www.twitch.tv/webdeverin" target="_blank" rel="noreferrer">View Erin's coding streams</a>
             <a className={styles.defaultlink} href="https://www.linkedin.com/in/erin-carver/" target="_blank" rel="noreferrer">Connect on Linkedin</a>
         </div>
     )
